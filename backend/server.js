64716e2aeb21c15730ffb57e87838567af372f5b
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-31 14:01:13 +0000

    connected express app to Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
-const express = require ('express')
-
+require('dotenv').config()
+const express = require ('express');
+const mongoose = require('mongoose');
 
 // Create Express App
 const app = express()
 
-// Listen for Requests 
 
-app.listen(4000, () => {
-  console.log('listening on port 4000')
+// Routes
+
+app.get("/", (req, res) => {
+  res.json({mssg: 'welcome to the app'})
 })
+
+// Connect to Database
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    // Only listen for requests when connected
+    app.listen(4000, () => {
+    console.log('listening on port 4000')
+})  
+  })
+  .catch((error) => (
+    console.log(error)
+  ))
+
