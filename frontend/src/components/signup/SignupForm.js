a1dba8503e3e15013d2e9be08c1f5ef24fc3efbf
Nicola <nfhearn@gmail.com>
2023-02-01 16:42:40 +0000

    adds state and handler to signup form

--- frontend/src/components/signup/SignupForm.js
+++ frontend/src/components/signup/SignupForm.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
 
 const Signup = (signup) => {
+
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+
+  const handleSignUp = async (e) => {
+    const user = {}
+
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSignUp}>
       <h3>Sign Up</h3>
       <div className="mb-3">
         <label>First name</label>
@@ -11,11 +24,19 @@ const Signup = (signup) => {
           type="text"
           className="form-control"
           placeholder="First name"
+          onChange={(e) => setFirstname(e.target.value)}
+          value={firstname}
         />
       </div>
       <div className="mb-3">
         <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
+        <input 
+          type="text" 
+          className="form-control" 
+          placeholder="Last name"
+          onChange={(e) => setLastname(e.target.value)}
+          value={lastname}
+          />
       </div>
       <div className="mb-3">
         <label>Email address</label>
@@ -23,6 +44,8 @@ const Signup = (signup) => {
           type="email"
           className="form-control"
           placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
       </div>
       <div className="mb-3">
@@ -31,10 +54,12 @@ const Signup = (signup) => {
           type="password"
           className="form-control"
           placeholder="Enter password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={email}
         />
       </div>
       <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary" >
           Sign Up
         </button>
       </div>
