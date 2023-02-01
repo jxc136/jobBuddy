27d859a321014749963a74dcf4deef024856bf82
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-01 13:04:19 +0000

    create applications router file

--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -1,1 +1,13 @@
+// Require express package 
+const express = require ('express')
 const applications = require ('../controllers/applicationController')
+
+// Create a new instance of the router
+const router = express.Router(); 
+
+// attach a route handler 
+router.get('/', (req, res) => {
+  res.JSON({mssg: 'get all applications'})
+});
+
+module.exports = router;
