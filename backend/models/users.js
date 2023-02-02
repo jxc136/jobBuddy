40a36811b9456f41f7bf2711d86db7e3214cac84
Nicola <nfhearn@gmail.com>
2023-02-02 13:11:59 +0000

    installs bcrypt

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt')
 
 
 const userSchema = new Schema({
@@ -23,6 +24,16 @@ const userSchema = new Schema({
   myApplications: []
 });
 
+userSchema.statics.signup = async function(firstname, lastname, email, password) {
+ const exists = await this.findOne({ email })
+
+ if (exists) {
+  throw Error('Email already in use')
+ }
+
+
+
+}
 
 module.exports = mongoose.model('User', userSchema)
 
