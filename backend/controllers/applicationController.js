14e2ff373745bda76d31a94f663db12b384f6b4c
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-03 14:47:26 +0000

    Create controller method for getting a single application

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -11,6 +11,25 @@ const getApplications = async (req, res) => {
   res.status(200).json(application)
 }
 
+// Get ONE application
+
+const getOneApplication = async (req, res) => {
+  const {id} = req.params
+  try {
+
+    const application = await Application.findOne({id})
+    if (application){
+      return res.status(200).json(application)
+    } else {
+      return res.json({mssg: "application not found"})
+    }
+    
+  } catch(error) {
+    res.status(400).json({error: error.message})
+  }
+ 
+}
+
 // Post a single application 
 const createApplication = async (req, res) => {
   const {job_title, application_title, employer, deadline, deadline_type, status, contact_person} = req.body
@@ -25,6 +44,7 @@ const createApplication = async (req, res) => {
 }
 
 module.exports = {
+  getApplications,
+  getOneApplication,
   createApplication,
-  getApplications
 };
