5c27eefe0cc2e76961a2b6d9acd70c4d694b6510
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-05 16:45:33 +0000

    further slying adjustments

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -4,7 +4,7 @@ import "./navbar.css";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
         <Navbar.Brand href="#home">jobBuddy</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
