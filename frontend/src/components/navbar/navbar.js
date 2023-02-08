5b46d4595b39ddee0291fbd748a7961b11af5a2a
Josh Carvell <j_carvell@hotmail.co.uk>
2023-02-08 11:09:38 +0000

    add logout button to navbar that clears localstorage

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -2,15 +2,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav,  Navbar } from 'react-bootstrap';
 import "./navbar.css";
 
+
 function NavBar() {
+
+const loggedIn = window.localStorage.getItem("user_id");
+
+  const handleLogout = () => {
+    if (loggedIn) {
+      window.localStorage.removeItem("token");
+      window.localStorage.removeItem("user_id");
+    }
+    
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
         <Navbar.Brand href="/">jobBuddy</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/login">login</Nav.Link>
-            <Nav.Link href="/signup">sign-up</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/signup">Sign-Up</Nav.Link>
+            <Nav.Link href="/login" onClick={handleLogout}>Log Out</Nav.Link>
           
           </Nav>
         </Navbar.Collapse>
