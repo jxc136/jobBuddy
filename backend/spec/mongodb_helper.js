7321d56c8e4809c6124d5cacda96b733360e4256
Nicola <nfhearn@gmail.com>
2023-02-08 15:50:44 +0000

    changes mongoose connection to use MONGO_URI in mongodb_helper

--- backend/spec/mongodb_helper.js
+++ backend/spec/mongodb_helper.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
+require("dotenv").config();
+
 
 beforeAll( (done) => {
-    mongoose.connect("mongodb://127.0.0.1/jobBuddy_test", {
+    mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -9,8 +11,8 @@ beforeAll( (done) => {
   var db = mongoose.connection;
   const users = db.collection('users')
   users.deleteMany({})
-  const applications = db.collection('applications')
-  applications.deleteMany({})
+  // const applications = db.collection('applications')
+  // applications.deleteMany({})
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
   db.on("open", function () {
     done();
